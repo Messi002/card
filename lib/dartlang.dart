cd594d3aa2f223d40d4a147c6db962e581c074f3
phantom <rostandmessi2@gmail.com>
2022-03-09 03:14:54 +0100

    bottom2

--- lib/dartlang.dart
+++ lib/dartlang.dart
@@ -1,7 +1,83 @@
 
 
-void main(){
-var full=['car','bus','plane', 'sweets'];
-print(full.isEmpty);
-print(full.length);
+// void main(){
+// var full=['car','bus','plane', 'sweets'];
+// print(full.isEmpty);
+// print(full.length);
+// }
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key ? key}): super (key: key);
+  static const String title='MIRsqaured Tech';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      home: RosT(),
+    );
+  }
+}
+
+class RosT extends StatefulWidget{
+  const RosT({Key?key}):super(key: key);
+  @override
+  _RosTState createState() => _RosTState();
+}
+
+class _RosTState extends State<RosT>{
+  int _selectedIndex=0;
+  static const String title='MIRsqaured Tech';
+  // _widgetOptions.elementAt(_selectedIndex),
+  static const TextStyle optionStyle= TextStyle(
+      fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions= <Widget>[
+    Text('Index 0:Home', style: optionStyle,),
+    Text('Index 1:Business', style: optionStyle,),
+    Text('Index 2:Account', style: optionStyle,),
+    Text('Index 3:Add photo', style: optionStyle,),
+    Text('Index 4:Setting', style: optionStyle,),
+  ];
+  void Tapped(int index){
+    setState(() {
+      _selectedIndex=index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text(title),),
+      body:
+      Center(child:_widgetOptions.elementAt(_selectedIndex),),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+
+        selectedItemColor: Colors.amberAccent,
+        unselectedItemColor: Colors.white,
+        onTap: Tapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+            backgroundColor: Colors.black87,),
+          BottomNavigationBarItem(
+              label: 'Business',
+              icon: Icon(Icons.business), backgroundColor: Colors.black87),
+          BottomNavigationBarItem(
+              label: 'Account',
+              icon: Icon(Icons.account_circle), backgroundColor: Colors.black87),
+          BottomNavigationBarItem(
+              label: 'Add photo',
+              icon: Icon(Icons.add_a_photo_outlined), backgroundColor: Colors.black87),
+          BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),backgroundColor: Colors.black87)
+        ],
+      ),
+    );
+  }
 }
--- lib/test widget.dart
+++ lib/test widget.dart
@@ -1,101 +1,75 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-
-
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget{
-  const MyApp({Key ? key}): super(key: key);
-  static const _title='MIRsquared Tech';
+  const MyApp({Key ? key}): super (key: key);
+  static const String title='MIRsqaured Tech';
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     theme: ThemeData(primarySwatch: Colors.pink,visualDensity: VisualDensity.adaptivePlatformDensity),
-     title:_title ,
-     debugShowCheckedModeBanner: false,
-     home: Scaffold(
-       appBar: AppBar(title: const Text(_title),),
-       body: Material(child:Ros(),),
-
-     ),
-   );
+  return MaterialApp(
+    title: title,
+    home: RosT(),
+  );
   }
 }
 
-
-//ignore
-class Ros extends StatefulWidget {
-  const Ros({Key ? key}) : super(key: key);
+class RosT extends StatefulWidget{
+  const RosT({Key?key}):super(key: key);
   @override
-  _RosState  createState() => _RosState();
+  _RosTState createState() => _RosTState();
 }
 
-class _RosState extends State<Ros>{
-
+class _RosTState extends State<RosT>{
+  int _selectedIndex=0;
+  static const String title='MIRsqaured Tech';
+  // _widgetOptions.elementAt(_selectedIndex),
+  static const TextStyle optionStyle= TextStyle(
+    fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions= <Widget>[
+    Text('Index 0:Home', style: optionStyle,),
+    Text('Index 1:Business', style: optionStyle,),
+    Text('Index 2:Account', style: optionStyle,),
+    Text('Index 3:Add photo', style: optionStyle,),
+    Text('Index 4:Setting', style: optionStyle,),
+  ];
+  void Tapped(int index){
+    setState(() {
+      _selectedIndex=index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-   return ListView(
-     padding: const EdgeInsets.all(8),
+  return Scaffold(
+      appBar: AppBar(title: const Text(title),),
+    body:
+    Center(child:_widgetOptions.elementAt(_selectedIndex),),
+    bottomNavigationBar: BottomNavigationBar(
+     currentIndex: _selectedIndex,
 
-     children: <Widget>[
-       Container(
-         padding: const EdgeInsets.all(15),
-         color:Colors.teal[100],
-         child: const ListTile(
-           leading: Icon(Icons.settings),
-           title: Text('Settings'),
-           trailing: Icon(Icons.arrow_forward_ios),
-         ),
-
-       ),
-       Card(
-         elevation: 16,
-         borderOnForeground: true,
-         clipBehavior: Clip.antiAlias,
-         semanticContainer: true,
-         child: const ListTile(
-           leading: Icon(Icons.wifi),
-           title: Text('Wifi'),
-           trailing: Icon(Icons.arrow_forward_ios),
-         ),
-         color:Colors.teal[200],
-       ),
-
-       Card(
-         borderOnForeground: true,
-         elevation: 6,
-         semanticContainer: true,
-         clipBehavior: Clip.antiAliasWithSaveLayer,
-         child:Container(
-         padding: const EdgeInsets.all(1),
-         child: const ListTile(
-           leading: Icon(Icons.accessibility_new_outlined),
-           title: Text('Accessibility'),
-           trailing: Icon(Icons.arrow_forward_ios),
-         ),
-         color:Colors.teal[300],
-       ),),
-
-       Container(
-         padding: const EdgeInsets.all(8),
-         child: const Text('Who scream'),
-         color:Colors.teal[400],
-       ),
-
-       Container(
-         padding: const EdgeInsets.all(8),
-         child: const Text('Revolution is coming'),
-         color:Colors.teal[500],
-       ),
-
-       Container(
-         padding: const EdgeInsets.all(8),
-         child: const Text('Revolution, they...'),
-         color:Colors.teal[600],
-       ),
-       ],
-   );
+      selectedItemColor: Colors.amberAccent,
+      unselectedItemColor: Colors.white,
+      onTap: Tapped,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          backgroundColor: Colors.black87,),
+        BottomNavigationBarItem(
+            label: 'Business',
+            icon: Icon(Icons.business), backgroundColor: Colors.black87),
+        BottomNavigationBarItem(
+            label: 'Account',
+            icon: Icon(Icons.account_circle), backgroundColor: Colors.black87),
+        BottomNavigationBarItem(
+            label: 'Add photo',
+            icon: Icon(Icons.add_a_photo_outlined), backgroundColor: Colors.black87),
+        BottomNavigationBarItem(
+        label: 'Settings',
+            icon: Icon(Icons.settings),backgroundColor: Colors.black87)
+      ],
+  ),
+  );
   }
 }
